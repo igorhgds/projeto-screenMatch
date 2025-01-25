06bf0945b000a4caf74e8b72d5491655a60df035
Igor Henrique <61789282+igorhgds@users.noreply.github.com>
2025-01-25 13:31:28 -0300

    lidando com herança e polimorfismo

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,6 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -6,13 +8,30 @@ public class Principal {
         meuFilme.setNome("Se Beber Não Case");
         meuFilme.setAnoDeLancamento(2009);
         meuFilme.setDuracaoEmMinutos(100);
-
         meuFilme.exibeFichaTecnica();
-        meuFilme.avalia(8);
-        meuFilme.avalia(5);
-        meuFilme.avalia(10);
+        System.out.println("Duração em minutos: " + meuFilme.getDuracaoEmMinutos());
 
-        System.out.println("Total de Avaliações: " + meuFilme.getTotalDeAvaliacoes());
-        System.out.println(meuFilme.mediaDasAvaliacoes());
+        Filme filmeFavorito = new Filme();
+        filmeFavorito.setNome("Gente Grande");
+        filmeFavorito.setAnoDeLancamento(2008);
+        filmeFavorito.setDuracaoEmMinutos(120);
+        filmeFavorito.exibeFichaTecnica();
+        System.out.println("Duração em minutos: " + filmeFavorito.getDuracaoEmMinutos());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2004);
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(30);
+        lost.exibeFichaTecnica();
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(filmeFavorito);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
+
     }
 }
